00df819732e3c3d2794d52f1c013cb8c4d8119d6
Oscar <null>
2019-12-14 13:02:05 +0100

    Fixed timer component and stylized it. Corrected background color.

--- src/app/infotimer/infotimer.component.ts
+++ src/app/infotimer/infotimer.component.ts
@@ -1,20 +1,23 @@
 import { Component, OnInit } from '@angular/core';
-import { DatePipe } from '@angular/common';
+
+class infoobject {
+  text: string;
+  time: number;
+}
 
 @Component({
   selector: 'app-infotimer',
   templateUrl: './infotimer.component.html',
   styleUrls: ['./infotimer.component.scss']
 })
+
 export class InfotimerComponent implements OnInit {
   list = [
     {
-      header: "Testevent",
-      body: "text eller hur.",
+      text: "text eller hur.",
       time: Date.now() + 1000
     },{
-      header: "Testevent",
-      body: "text eller hur.",
+      text: "Borrowed from German Fest. Related to Middle English feste, from Old French feste, from Latin festum however, the modern word is a borrowing of the German, which shares the same ultimate origin. More at feast.",
       time: 121333344
     }
   ]
@@ -29,10 +32,4 @@ export class InfotimerComponent implements OnInit {
   timeLeft(info: infoobject){
     return Date.now() - info.time;
   }
-}
-
-class infoobject {
-  header: string;
-  body: string;
-  time: number;
 }
