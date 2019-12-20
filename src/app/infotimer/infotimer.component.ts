7d3d2c9de565198b22d83e16872bb26f32b78694
Oscar <oscar.andersson.200@gmail.com>
2019-12-20 14:25:52 +0100

    Datatype changes from number to date on variable time in timer component.

--- src/app/infotimer/infotimer.component.ts
+++ src/app/infotimer/infotimer.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 class InfoObject {
   text: string;
-  time: number;
+  time: Date;
 }
 
 @Component({
@@ -16,23 +16,24 @@ export class InfotimerComponent implements OnInit {
   list = [
     {
       text: 'Nu smäller det, Campusfesten är här',
-      time: new Date( 'May 28, 2020, 15:00' ).getTime()
+      time: new Date( '01 28, 2020, 15:00' )
     },
     {
-      // tslint:disable-next-line:max-line-length
       text: 'Borrowed from German Fest. Related to Middle English feste, from Old French feste, from Latin festum however, the modern word is a borrowing of the German, which shares the same ultimate origin. More at feast.',
-      time: 121333344
+      time: new Date( 'May 28, 2019, 00:00' )
     }
   ];
 
   constructor() {
-    console.log('current time ' + Date.now());
+    this.list.forEach(e => {
+      console.log(e.time + ":" + this.timeLeft(e) + " -> " + e.text);
+    });
   }
 
   ngOnInit() {
   }
 
   timeLeft(info: InfoObject) {
-    return Date.now() - info.time;
+    return Date.now() - info.time.getTime();
   }
 }
