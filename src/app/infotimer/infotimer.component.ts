b60803bd1ad485eb6832e78c9c7baaeb39b913aa
Oscar Andersson <oscar.andersson.200@gmail.com>
2020-01-12 13:22:54 +0100

    Remaster of infotimer logic.

--- src/app/infotimer/infotimer.component.ts
+++ src/app/infotimer/infotimer.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
-class InfoObject {
+class EventObject {
   text: string;
+  textEn: string;
   time: Date;
 }
 
@@ -12,39 +13,41 @@ class InfoObject {
 })
 
 
-export class InfotimerComponent implements OnInit {
-  list = [
+export class InfotimerComponent {
+  eventsUnreleased: EventObject;
+  eventsReleased: Array<EventObject> = [];
+  eventsAll: Array<EventObject> = [
     {
-      text: 'Nu smäller det, Campusfesten är här',
+      text: 'Campusfesten är igång! Gå till campus!',
+      textEn: 'The annual Campus party is on right now! Head out to campus!',
       time: new Date( '05 30, 2020, 15:00' )
-    },
+    },{
+      text: 'Valborg',
+      textEn: 'Valborg',
+      time: new Date( '04 30, 2020, 15:00' )
+    },{
+      text: 'Nu',
+      textEn: 'Now',
+      time: new Date( '01 05, 2020, 15:00' )
+    }
   ];
-  ready = [];
-  closest = 0;
-  object = {};
 
   constructor() {
-    this.list.forEach(e => {
-      const temp = this.timeLeft( e );
-      if (this.timeLeft(e) > 0) {
-        this.ready.push( e );
-      } else {
-        if (this.closest === 0) {
-          this.closest = temp;
-          this.object = e;
-        } else if (temp > this.closest) {
-          this.closest = temp;
-          this.object = e;
+    // Sort the "eventsAll" list by release date
+    this.eventsAll.sort((a: EventObject, b: EventObject) => {
+      return b.time.getTime() - a.time.getTime();
+    });
+
+    // Copy released and unreleased events to respective list
+    for(let i = 0; i < this.eventsAll.length; i++){
+      let event = this.eventsAll[i];
+      if(event.time.getTime() <= Date.now()){
+        this.eventsReleased.push(event);
+      }else{
+        if(this.eventsUnreleased == undefined || this.eventsUnreleased.time.getTime() > event.time.getTime()){
+          this.eventsUnreleased = event;
         }
       }
-    });
-    this.ready.unshift( this.object );
-  }
-
-  ngOnInit() {
-  }
-
-  timeLeft(info: InfoObject) {
-    return Date.now() - info.time.getTime();
+    }
   }
 }
